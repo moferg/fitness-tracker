167617f0d03f196c7cd4e01d94d60411a306ee67
Marshall Ferguson <fergusonmarshall0@gmail.com>
2022-01-06 12:23:04 -0500

    added execute_query function

--- testing/database_prep_test.py
+++ testing/database_prep_test.py
@@ -15,3 +15,11 @@ def create_connection(path):
 
     return connection
 
+def execute_query(connection, query):
+    cursor = connection.cursor()
+    try:
+        cursor.execute(query)
+        connection.commit()
+        print('Query executed successfully')
+    except Error as e:
+        print(f'The error {e} occurred')
