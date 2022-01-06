1ae07fd1ba3f77558da02caabffd4b92d0e53093
Marshall Ferguson <fergusonmarshall0@gmail.com>
2022-01-06 12:24:08 -0500

    added execute_read_query function

--- testing/database_prep_test.py
+++ testing/database_prep_test.py
@@ -23,3 +23,13 @@ def execute_query(connection, query):
         print('Query executed successfully')
     except Error as e:
         print(f'The error {e} occurred')
+
+def execute_read_query(connection, query):
+    cursor = connection.cursor()
+    result = None
+    try:
+        cursor.execute(query)
+        result = cursor.fetchall()
+        return result
+    except Error as e:
+        print(f'The error {e} occurred')
